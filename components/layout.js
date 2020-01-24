fad1ce39ccc93f6a79ee610e2f397592e3d95d16
Nicholas Pellizer <nicholasp21@gmail.com>
2020-01-24 01:52:44 +0100

    add initial next js tutorial

---
+++ components/layout.js
@@ -0,0 +1,18 @@
+import Header from './header';
+
+export default function Layout (props){
+    
+    const layoutStyle = {
+        margin: 20,
+        padding: 20,
+        border: '1px solid #DDD'
+    };
+
+    return (
+        <div style={layoutStyle} >
+            <Header />
+            {props.children}
+        </div >
+    )
+};
+
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Header from '../components/header'
+import Layout from '../components/layout'
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <p>
+                    developed with love with React and Next by DanD Studios
+                </p>
+            </div>
+        </Layout>
+    )
+}
