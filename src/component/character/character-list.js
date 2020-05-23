087ba602dd11e742b0bed9ae7c417851cd0cd1bf
Nicholas Pellizer <nicholasp21@gmail.com>
2020-05-23 19:59:36 +0200

    margin in list

--- src/component/character/character-list.js
+++ src/component/character/character-list.js
@@ -6,7 +6,8 @@ export default class CharacterList extends React.Component {
 
     const charlist = this.props.characters.map(c => (
       <Card interactive={true} key={c._id}
-        onClick={(e) => this.props.selectCharacter(c, e)}>
+        onClick={(e) => this.props.selectCharacter(c, e)}
+        className='list'>        
         <small>{c.class}</small>
         <p>{c.name}</p>
       </Card>
